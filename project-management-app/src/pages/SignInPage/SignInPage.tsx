65d99f700ba5ba7c6dfdad00cbe158a923fff76f
Anastasia Arzhanik <Anastasia911@mail.ru>
2022-05-07 12:01:57 +0300

    feat: add signinPage

--- project-management-app/src/pages/SignInPage/SignInPage.tsx
+++ project-management-app/src/pages/SignInPage/SignInPage.tsx
@@ -1,7 +1,65 @@
-export function SignInPage() {
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { ToastContainer } from 'react-toastify';
+import { useAppDispatch } from '../../store/hooks';
+import { authUser } from '../../thunks/user';
+import { setUserInfo } from '../../reducers/userReducer';
+import Input from '../../components/Input/Input';
+import './../SignUpPage/signUpPage.css';
+import './signinPage.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+export type IUser = {
+  login: string;
+  password: string;
+};
+
+export const SignInPage = () => {
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm<IUser>({
+    mode: 'onChange',
+  });
+
+  const onSubmit: SubmitHandler<IUser> = (data) => {
+    const login = {
+      login: data.login,
+      password: data.password,
+    };
+    dispatch(authUser(login));
+    dispatch(setUserInfo(login));
+  };
+
   return (
-    <div>
-      <p>SignIn Page</p>
+    <div className="container-form">
+      <div className="container-img-autorisation"></div>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <h2>Log In</h2>
+        <Input
+          register={register('login')}
+          nameInput={'login'}
+          textLabel={'Login'}
+          datatestId={'input-login'}
+          type={'text'}
+        />
+        <Input
+          register={register('password')}
+          nameInput={'password'}
+          textLabel={'Password'}
+          datatestId={'input-password'}
+          type="password"
+          autoComplete="off"
+        />
+        <div className="redirect">
+          New User? <a>Sign Up</a>
+        </div>
+        <button
+          data-testid="button-submit-form"
+          className="form__btn-submit"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          Log In
+        </button>
+      </form>
+      <ToastContainer position="top-right" />
     </div>
   );
-}
+};
