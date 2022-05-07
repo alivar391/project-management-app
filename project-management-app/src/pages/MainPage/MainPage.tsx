bfca93965e7768e91f9957685ae492f8dcf26f4b
Diana26 <plohayadamochka@gmail.com>
2022-05-07 19:05:10 +0300

    feat main page view

--- project-management-app/src/pages/MainPage/MainPage.tsx
+++ project-management-app/src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,29 @@
 import { Header } from '../../components/Header/Header';
+import './main-page.css';
 
 export function MainPage() {
   return (
     <div>
       <Header />
-      <p>Main Page</p>
+      <div className="main">
+        <div className="main__header">
+          <span className="main__title">Good day, Diana!</span>
+          <button className="main__btn">Create board</button>
+        </div>
+        <div className="main__cont">
+          <div className="board">
+            <div className="board__view"></div>
+            <div className="board__buttns">
+              <span className="icon-button">
+                <img src="./../assets/jpg/pencil.png" alt="icon-file" />
+              </span>
+              <span className="icon-button">
+                <img src="./../assets/jpg/trash.png" alt="icon-file" />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
