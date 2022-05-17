7f037072d248086678799f8e543de228cf4b875a
Anastasia Arzhanik <Anastasia911@mail.ru>
2022-05-17 21:11:26 +0300

    feat:add errorBoundary

--- project-management-app/src/App.tsx
+++ project-management-app/src/App.tsx
@@ -10,28 +10,31 @@ import { Page404 } from './pages/Page404/Page404';
 
 import { Layout } from './components/Layout/Layout';
 import { RequireAuth } from './hoc/RequireAuth';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route
-            index
-            element={
-              <RequireAuth>
-                <MainPage />
-              </RequireAuth>
-            }
-          />
-          <Route path=":boardId" element={<BoardPage />} />
-          <Route path="welcome" element={<WelcomePage />} />
-          <Route path="signin" element={<SignInPage />} />
-          <Route path="signup" element={<SignUpPage />} />
-          <Route path="update-user" element={<UpdateUserPage />} />
-          <Route path="*" element={<Page404 />} />
-        </Route>
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <RequireAuth>
+                  <MainPage />
+                </RequireAuth>
+              }
+            />
+            <Route path=":boardId" element={<BoardPage />} />
+            <Route path="welcome" element={<WelcomePage />} />
+            <Route path="signin" element={<SignInPage />} />
+            <Route path="signup" element={<SignUpPage />} />
+            <Route path="update-user" element={<UpdateUserPage />} />
+            <Route path="*" element={<Page404 />} />
+          </Route>
+        </Routes>
+      </ErrorBoundary>
     </div>
   );
 }
