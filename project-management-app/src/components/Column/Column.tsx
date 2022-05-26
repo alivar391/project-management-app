cbed2e3515ab883428e9503c3a432af54d40ced1
alivar391 <patriot391@mail.ru>
2022-05-26 15:28:27 +0300

    feat: add styles when dragging columns

--- project-management-app/src/components/Column/Column.tsx
+++ project-management-app/src/components/Column/Column.tsx
@@ -69,7 +69,7 @@ export const Column = ({ boardId, column }: IColumnProps) => {
     }),
   });
 
-  const [, dropRef] = useDrop({
+  const [{ isOver, canDrop }, dropRef] = useDrop({
     accept: ['column', 'task'],
     async drop(item: IColumnProps | ITaskProps) {
       if ((item as IColumnProps).column) {
@@ -100,7 +100,21 @@ export const Column = ({ boardId, column }: IColumnProps) => {
         await dispatch(getBoard({ boardId }));
       }
     },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
   });
+
+  const isActive = isOver && canDrop;
+  let backgroundColor = '#a7d1f8';
+  let border = 'none';
+  if (isActive) {
+    backgroundColor = '#4ea1f0';
+    border = '1px dashed black';
+  } else if (canDrop) {
+    backgroundColor = '#85bff5';
+  }
 
   dragRef(dropRef(refColumn));
 
@@ -119,7 +133,13 @@ export const Column = ({ boardId, column }: IColumnProps) => {
 
   return (
     <>
-      <li key={id} className="column" id={`column-${order}`} ref={refColumn} style={{ opacity }}>
+      <li
+        key={id}
+        className="column"
+        id={`column-${order}`}
+        ref={refColumn}
+        style={{ opacity, backgroundColor, border }}
+      >
         <div className="column__header">
           {isInput ? (
             <InputTitle setIsInput={setIsInput} column={column} boardId={boardId} />
