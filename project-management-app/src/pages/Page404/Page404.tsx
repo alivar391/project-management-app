f77c70acd688454c31497d4554773dc3e632325c
alivar391 <patriot391@mail.ru>
2022-05-29 12:08:25 +0300

    feat: add styles to 404 page

--- project-management-app/src/pages/Page404/Page404.tsx
+++ project-management-app/src/pages/Page404/Page404.tsx
@@ -1,7 +1,10 @@
+import './Page404.css';
+
 export function Page404() {
   return (
-    <div>
-      <p>404 Page</p>
+    <div className="page404__container">
+      <p className="text404">404</p>
+      <p className="text-404-page">Page not found</p>
     </div>
   );
 }
